7cab1ca857e7ee2657330cb6943781225de6b3a8
asafeisenberg <asaftrvl@yahoo.com>
2022-04-25 02:40:48 +0300

    feat: open popup

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 let editButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".form__close-button");
+let popup = document.querySelector(".popup");
+
+function activePopup() {
+  popup.classList.add("popup_opened");
+}
+function removePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+editButton.addEventListener("click", activePopup);
+closeButton.addEventListener("click", removePopup);
