308b30cb0c77a16c2a295e277f3473588b12041d
asafeisenberg <asaftrvl@yahoo.com>
2022-04-28 22:47:24 +0300

    feat: make page responsive2

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-let editButton = document.querySelector(".profile__edit-button");
-let closeButton = document.querySelector(".form__close-button");
-let popup = document.querySelector(".popup");
+// queryselectors
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".form__close-button");
+const popup = document.querySelector(".popup");
+const saveButton = document.querySelector(".popup__submit-button");
 
+// functions
 function activePopup() {
   popup.classList.add("popup_opened");
 }
 function removePopup() {
   popup.classList.remove("popup_opened");
 }
-
+function saveText() {
+  console.log("text saved!");
+}
+// eventlisteners
 editButton.addEventListener("click", activePopup);
 closeButton.addEventListener("click", removePopup);
+saveButton.addEventListener("click", saveText);
