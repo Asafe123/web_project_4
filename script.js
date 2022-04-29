b353f30d4145a6b455e32cc8c5148f0c8bbc9793
asafeisenberg <asaftrvl@yahoo.com>
2022-04-29 16:43:20 +0300

    js arranged

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".form__close-button");
 const popup = document.querySelector(".popup");
-const saveButton = document.querySelector(".popup__submit-button");
+const profileForm = document.querySelector(".popup__form_type_profile");
+const profileName = document.querySelector(".profile__title");
+const profileOccupation = document.querySelector(".profile__ocupation");
+const profileInputName = document.querySelector(".popup__form-input_type_name");
+const profileInputOccupation = document.querySelector(
+  ".popup__form-input_type_occupation"
+);
+
 // functions
 function activePopup() {
   popup.classList.add("popup_opened");
@@ -10,11 +17,15 @@ function activePopup() {
 function removePopup() {
   popup.classList.remove("popup_opened");
 }
-function saveText() {
-  const myText = "This can be whatever text you like!";
-  alert(myText);
+function saveProfileForm(evt) {
+  //erase defualt settings
+  evt.preventDefault();
+  // remove popup
+  removePopup(popup);
+  profileName.textContent = profileInputName.value;
+  profileOccupation.textContent = profileInputOccupation.value;
 }
 // eventlisteners
 editButton.addEventListener("click", activePopup);
 closeButton.addEventListener("click", removePopup);
-saveButton.addEventListener("submit", saveText);
+profileForm.addEventListener("submit", saveProfileForm);
