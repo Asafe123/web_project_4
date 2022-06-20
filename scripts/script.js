f2471f659f0388f9e557e64e7778d869f35bf44a
asafeisenberg <asaftrvl@yahoo.com>
2022-06-20 11:57:11 +0300

    feat:make image form

--- scripts/script.js
+++ scripts/script.js
@@ -25,11 +25,16 @@ const initialCards = [
     link: "https://code.s3.yandex.net/web-code/lago.jpg",
   },
 ];
-//wrappers querySelectors
+//////////////////////////////
+///wrappers///////////////////
+//////////////////////////////
 const placesList = document.querySelector(".places__list");
 
-// queryselectors
-const editButton = document.querySelector(".profile__edit-button");
+/////////////////////
+// queryselectors////
+/////////////////////
+const editProfileButton = document.querySelector(".profile__edit-button");
+const addImageButton = document.querySelector(".profile__add-button");
 const profileName = document.querySelector(".profile__name");
 const profileOccupation = document.querySelector(".profile__occupation");
 const closeButton = document.querySelector(".popup__close-button");
@@ -39,12 +44,18 @@ const profileInputName = document.querySelector(".popup__form-input_type_name");
 const profileInputOccupation = document.querySelector(
   ".popup__form-input_type_occupation"
 );
+const popupImageEditor = document.querySelector(".popup_image_editor");
+/////////////////////
+// functions/////////
+/////////////////////
 
-// functions
 function activePopup() {
   popup.classList.add("popup_opened");
   profileInputName.value = profileName.textContent;
   profileInputOccupation.value = profileOccupation.textContent;
+}
+function activeAddImagePopup() {
+  popupImageEditor.classList.add("popup_opened");
 }
 function closePopup() {
   popup.classList.remove("popup_opened");
@@ -52,21 +63,28 @@ function closePopup() {
 function saveProfileForm(evt) {
   //erase defualt settings
   evt.preventDefault();
-  // close popup
+  //close popup
   closePopup();
   //enter values on profile window
   profileName.textContent = profileInputName.value;
   profileOccupation.textContent = profileInputOccupation.value;
 }
-//EVENT LISTENERS
-editButton.addEventListener("click", activePopup);
+//////////////////////
+//EVENT LISTENERS/////
+//////////////////////
+editProfileButton.addEventListener("click", activePopup);
+addImageButton.addEventListener("click", activeAddImagePopup);
 closeButton.addEventListener("click", closePopup);
 profileForm.addEventListener("submit", saveProfileForm);
 
-//TEMPLATES
+//////////////////////
+//TEMPLATES///////////
+//////////////////////
+
 const cardTemplate = document
   .querySelector("#card-template")
   .content.querySelector(".places__item");
+
 initialCards.forEach(function (card) {
   //clone template
   const cardElement = cardTemplate.cloneNode(true);
