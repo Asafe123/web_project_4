6e4c02a4caa20c4411f7c3d458b381583bd70f1a
asafeisenberg <asaftrvl@yahoo.com>
2022-08-02 01:15:25 +0300

    make js alternative  file

---
+++ scripts/script2.js
@@ -0,0 +1,43 @@
+alert("asaf, its time to sleep");
+
+const initialCards = [
+  {
+    title: "Tel-Aviv Landscape",
+    link: "https://i0.wp.com/www.touristisrael.com/wp-content/uploads/2020/06/Best-areas-to-stay-in-Tel-Aviv-scaled-e1593008399620.jpg?w=1506&ssl=1",
+  },
+  {
+    title: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    title: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    title: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    title: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    title: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
+const templateListItem = document
+  .querySelector(".card-template")
+  .content.querySelector(".card");
+const list = document.querySelector(".places__list");
+
+const listItem = templateListItem.cloneNode(true);
+
+const title = listItem.querySelector(".card__title");
+const image = listItem.querySelector(".card__image");
+
+title.textContent = "qwerty";
+image.style.backgroundImage = `url(https://i0.wp.com/www.touristisrael.com/wp-content/uploads/2020/06/Best-areas-to-stay-in-Tel-Aviv-scaled-e1593008399620.jpg?w=1506&ssl=1)`;
+list.append(listItem);
+const listItem = "123";
