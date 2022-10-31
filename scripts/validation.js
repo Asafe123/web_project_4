e35430ed1cf8b023a489dd6ea5ca0741d6d7a5ee
asafeisenberg <asaftrvl@yahoo.com>
2022-10-31 21:24:20 +0200

    feat:make error message

---
+++ scripts/validation.js
@@ -0,0 +1,53 @@
+function showError(input) {
+  const error = input.validationMessage;
+  const errorElement = document.querySelector(`#${input.id}-error`);
+  errorElement.textContent = error;
+}
+function hideError(input) {
+  const errorElement = document.querySelector(`#${input.id}-error`);
+  errorElement.textContent = "";
+  input.classList.remove("popup__input_theme_error");
+}
+
+function checkValidity(input) {
+  if (input.validity.valid) {
+    hideError(input);
+  } else {
+    showError(input);
+  }
+}
+
+function enableValidation(settings) {
+  // find all forms
+  const forms = [...document.querySelectorAll(".popup__form")];
+  console.log(forms);
+  // prevent default
+  forms.forEach((form) => {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+
+    // search all inputs inside forms
+    const inputs = [...form.querySelectorAll(".popup__input")];
+    inputs.forEach((input) => {
+      input.addEventListener("input", () => {
+        console.log("who clicked here?");
+        checkValidity(input);
+      });
+    });
+  });
+  // subscribe to its change
+  // chack if input is valid
+  // if valid => no action needed
+  // if not valid => show error
+}
+
+const config = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+enableValidation(config);
